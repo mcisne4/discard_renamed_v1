7c8e1407601e37ce80056b1f93aa6892a6a5f821
mcisne4 <mcisne4@gmail.com>
2023-07-31 16:32:07 -0500

    Svelte: Edit app for testing

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,17 @@
 <script lang="ts">
-  import Greet from "./lib/Greet.svelte";
+  //
 </script>
 
-<main class="container">
-  <h1>Welcome to Tauri!</h1>
-
-  <div class="row">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-
-  <p>Click on the Tauri, Vite, and Svelte logos to learn more.</p>
-
-  <div class="row">
-    <Greet />
-  </div>
+<main>
+  <h1>Hello WOrld</h1>
 </main>
 
-<style>
-  .logo.vite:hover {
-    filter: drop-shadow(0 0 2em #747bff);
-  }
-
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00);
+<style lang="scss">
+  main {
+    background-color: orangered;
+    width: 100%;
+    height: 100%;
+    min-height: 100vh;
+    min-width: 100vw;
   }
 </style>
